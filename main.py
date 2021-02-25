3ea989c48922332942074e051a7a06c1f5132c0d
zysoong <75294755+zysoong@users.noreply.github.com>
2021-02-25 18:50:20 +0100

    Update main.py
    Change code to fit DIOR dataset

--- main.py
+++ main.py
@@ -3,16 +3,10 @@ import json
 from os import listdir, getcwd
 from os.path import join
 
-classes = ["person","bicycle","car","motorcycle","airplane","bus","train",
-	   "truck","boat","traffic light","fire hydrant","stop sign","parking meter",
-	   "bench","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra",
-	   "giraffe","backpack","umbrella","handbag","tie","suitcase","frisbee","skis",
-	   "snowboard","sports ball","kite","baseball bat","baseball glove","skateboard",
-	   "surfboard","tennis racket","bottle","wine glass","cup","fork","knife","spoon",
-	   "bowl","banana","apple","sandwich","orange","broccoli","carrot","hot dog","pizza",
-	   "donut","cake","chair","couch","potted plant","bed","dining table","toilet","tv",
-	   "laptop","mouse","remote","keyboard","cell phone","microwave","oven","toaster","sink",
-	   "refrigerator","book","clock","vase","scissors","teddy bear","hair drier","toothbrush"]
+classes = ["airplane","airport","baseballfield","basketballcourt","bridge","chimney","dam",
+          "Expressway-Service-area", "Expressway-toll-station", "golffield", "groundtrackfield", 
+          "harbor", "overpass", "ship", "stadium", "storagetank", "tenniscourt", "trainstation", 
+          "vehicle", "windmill"]
 
 #box form[x,y,w,h]
 def convert(size,box):
@@ -25,25 +19,25 @@ def convert(size,box):
     return (x,y,w,h)
 
 def convert_annotation():
-    with open('/media/yehao/ye/coco/annotations/instances_val2014.json','r') as f:
+    with open('../val.json','r') as f:
         data = json.load(f)
     for item in data['images']:
-	image_id = item['id']
-	file_name = item['file_name']
-	width = item['width']
-	height = item['height']
-	value = filter(lambda item1: item1['image_id'] == image_id,data['annotations'])
-        outfile = open('/media/yehao/ye/coco/labellabel/%s.txt'%(file_name[:-4]), 'a+')
+        image_id = item['id']
+        file_name = item['file_name']
+        width = item['width']
+        height = item['height']
+        value = filter(lambda item1: item1['image_id'] == image_id,data['annotations'])
+        outfile = open('../labels-trainval/%s.txt'%(file_name[:-4]), 'a+')
         for item2 in value:
-	    category_id = item2['category_id']
-	    value1 = filter(lambda item3: item3['id'] == category_id,data['categories'])
-	    name = value1[0]['name']
-	    class_id = classes.index(name)
-	    box = item2['bbox']
-	    bb = convert((width,height),box)
-	    outfile.write(str(class_id)+" "+" ".join([str(a) for a in bb]) + '\n')
-	outfile.close()
-			
+            category_id = item2['category_id']
+            value1 = list(filter(lambda item3: item3['id'] == category_id,data['categories']))
+            name = value1[0]['name']
+            class_id = classes.index(name)
+            box = item2['bbox']
+            bb = convert((width,height),box)
+            outfile.write(str(class_id)+" "+" ".join([str(a) for a in bb]) + '\n')
+        outfile.close()
+                        
 if __name__ == '__main__':
     convert_annotation()
 
